4257f4c6c4d40cf7b4de872cce5b289304cb955b
UnrealTemplier <jaimore.evseev@gmail.com>
2024-10-28 11:15:02 +0400

    Initial commit.

---
+++ onlyphp.php
@@ -0,0 +1,5 @@
+<?php
+
+$name = "Sergey";
+
+echo $name;
---
+++ test.php
@@ -0,0 +1,13 @@
+Hello, world
+
+<?php
+    echo "Hello, world!";
+?>
+
+Another string output
+
+<?php
+echo "Another php tag output...";
+?>
+
+Another another string output
