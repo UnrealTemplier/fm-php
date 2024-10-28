3d673c1aeab4ddef45cb6f34e2634b4d0267c530
UnrealTemplier <jaimore.evseev@gmail.com>
2024-10-28 20:19:22 +0400

    Submit form with GET request.

---
+++ Cryptomasters/convert.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<title>Conversion Results</title>
+
+<h1>Conversion Results</h1>
+
+<?php
+
+$amount = $_GET["amount"];
+$crypto = $_GET["crypto"];
+
+echo "<p>You want to convert $amount $crypto</p>";
--- Cryptomasters/index.php
+++ Cryptomasters/index.php
@@ -2,3 +2,14 @@
 <title>Crypto Masters</title>
 
 <h1>Crypto Masters</h1>
+
+<form action="convert.php">
+    <label for="amount">Amount</label>
+    <input id="amount" name="amount">
+    <label for="crypto">Cryptocurrency</label>
+    <select id="crypto" name="crypto">
+        <option>BTC</option>
+        <option>ETH</option>
+    </select>
+    <button type="submit">Convert</button>
+</form>
