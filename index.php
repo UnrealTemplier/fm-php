f558b6976085bce6b7d2fc952e2178fa4d31435a
UnrealTemplier <jaimore.evseev@gmail.com>
2024-10-28 19:32:50 +0400

    First development web-server attempts.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First index.php</title>
+</head>
+
+<body>
+    <h1>Are you PHP?</h1>
+
+    <?php
+    echo "Is this gonna work?";
+    ?>
+</body>
+
+</html>
